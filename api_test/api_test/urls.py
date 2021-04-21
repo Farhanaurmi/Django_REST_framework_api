8fba607557dfc7a6397f0c5c94994eba30cb89a2
Farhana islam urmi <76726981+Farhanaurmi@users.noreply.github.com>
2021-04-21 16:29:10 +0600

    added login option

--- api_test/api_test/urls.py
+++ api_test/api_test/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from posts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/posts',views.PostList.as_view()),
     path('api/posts/<int:pk>/vote',views.VoteList.as_view()),
+    path('api-auth/',include('rest_framework.urls'))
 ]
